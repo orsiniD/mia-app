8ad9263d6f433ad053b43dec878c12405e7a32c7
Diletta <dile.artes@hotmail.it>
2023-03-09 12:46:34 +0100

    finito esercizio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AnimaliComponent } from './animali/animali.component';
+import { PersoneComponent } from './persone/persone.component';
+import { PippoComponent } from './pippo/pippo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimaliComponent,
+    PersoneComponent,
+    PippoComponent
   ],
   imports: [
     BrowserModule
